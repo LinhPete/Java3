252e2e00ac6400d52b5bf28b69a590247938f348
Linh Pete <peterdamlinh1215@gmail.com>
2024-10-01 09:37:17 +0700

    enum

---
+++ src/main/java/util/other/Accounts.java
@@ -0,0 +1,21 @@
+package util.other;
+
+public enum Accounts {
+	SQL_ACC("","");
+	
+	private final String username;
+	private final String password;
+
+	Accounts(String username, String password) {
+		this.username = username;
+		this.password = password;
+	}
+	
+	public String getUsername() {
+		return username;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+}
