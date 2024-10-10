443b8ab12b2915c3414b8e98a178fd05b6594062
Linh Pete <peterdamlinh1215@gmail.com>
2024-10-10 10:43:02 +0700

    add lÃistener

---
+++ src/main/java/listeners/AppContextListener.java
@@ -0,0 +1,36 @@
+package listeners;
+
+import jakarta.servlet.ServletContextEvent;
+import jakarta.servlet.ServletContextListener;
+import jakarta.servlet.annotation.WebListener;
+import util.other.DataSourceFactory;
+
+/**
+ * Application Lifecycle Listener implementation class AppContextListener
+ *
+ */
+public class AppContextListener implements ServletContextListener {
+
+    /**
+     * Default constructor. 
+     */
+    public AppContextListener() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+     * @see ServletContextListener#contextInitialized(ServletContextEvent)
+     */
+    public void contextInitialized(ServletContextEvent sce)  { 
+         // TODO Auto-generated method stub
+    }
+
+	/**
+     * @see ServletContextListener#contextDestroyed(ServletContextEvent)
+     */
+    public void contextDestroyed(ServletContextEvent sce)  { 
+         // TODO Auto-generated method stub
+    	DataSourceFactory.closeDataSource();
+    }
+	
+}
